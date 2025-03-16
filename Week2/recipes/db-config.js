4d11ae8eb9a76fdb0468b8eb2842aee365de445e
konjit <kkll6349@gmail.com>
2025-03-16 21:39:59 +0100

    Complete prep-exercise w2 of DB.

---
+++ Week2/recipes/db-config.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import mysql from "mysql2/promise";
+
+dotenv.config();
+
+let pool;
+
+export const getPool = async () => {
+  if (!pool) {
+    pool = mysql.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+  }
+  return pool;
+};
+
+
+export default getPool;
---
+++ Week2/recipes/recipes.js
@@ -0,0 +1,77 @@
+import { getPool } from "./db-config.js";
+
+const executeQuery = async (query, params = []) => {
+  const pool = await getPool();
+
+  try {
+    const [result] = await pool.query(query);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const GET_ALL_RECIPES = `SELECT name from recipes;`;
+
+const recipes = await executeQuery(GET_ALL_RECIPES);
+
+const listIngredients = async (recipe) => {
+    const GET_INGREDIENTS = `
+    SELECT i.name AS ingredient_name
+    FROM recipes as r 
+    INNER JOIN recipe_ingredients AS ri ON r.id = ri.recipe_id
+    INNER JOIN ingredients AS i ON i.id = ri.ingredient_id
+    WHERE r.name = "${recipe}";
+    `;
+    return await executeQuery(GET_INGREDIENTS);
+};
+
+const listSteps = async (recipe) => {
+    const GET_STEPS = `
+    SELECT s.description AS instruction, rs.step_order AS orders
+    FROM recipes AS r 
+    INNER JOIN recipe_steps AS rs ON r.id = rs.recipe_id
+    INNER JOIN steps AS s ON s.id = rs.step_id
+    WHERE r.name = "${recipe}";
+    `;
+    return await executeQuery(GET_STEPS);
+};
+
+
+
+for (const { recipe } of recipes) {
+    const ingredients = await listIngredients(recipe);
+    const steps = await listSteps(recipe);
+
+    console.log(`\nIngredients for ${recipe}:`);
+    ingredients.forEach(i => {
+        console.log(`- ${i.ingredient_name}`);
+    });
+
+    console.log(`\nSteps for ${recipe}:`);
+    steps.sort((a, b) => a.orders - b.orders).forEach(step => {
+        console.log(`Step ${step.orders}: ${step.instruction}`);
+    });
+
+    console.log('\n' + '-'.repeat(40)); 
+}
+
+
+
+// const GET_RECIPES_BY_CATEGORY = `
+//     SELECT r.name, c.name FROM recipes as r 
+//     inner join recipe_categories as rc ON r.id=rc.recipe_id
+//     inner join categories as c on c.id=rc.category_id
+//     where c.name="Japanese;"`;
+
+export const closePool = async () => {
+  const pool = await getPool(process.env.DB_NAME_2);
+  try {
+    await pool.end();
+    console.log("Pool closed successfully.");
+  } catch (error) {
+    console.error("Error closing the pool:", error);
+  }
+};
+
+await closePool();
